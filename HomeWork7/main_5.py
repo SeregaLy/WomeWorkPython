8b5d1615ff58467245a9f654c64afca99601890b
SeregaLi <kaprall13rus@gmail.com>
2022-12-18 09:01:08 +0300

    HW7  final

--- HomeWork7/main_5.py
+++ HomeWork7/main_5.py
@@ -6,6 +6,8 @@
 # переопределение метода draw. Для каждого из классов метод должен выводить
 # уникальное сообщение. Создать экземпляры классов и проверить, что выведет
 # описанный метод для каждого экземпляра.
+from sys import argv
+
 
 class Stationary:
     def __init__(self, title):
@@ -20,7 +22,7 @@ class Pen(Stationary):
         super().__init__(title)
 
     def draw(self):
-        return f'Вы взяли {self.title}. Запуск отрисовки ручкой'
+        return f'Запуск отрисовки. {self.title} хорошо пишет'
 
 
 class Pencil(Stationary):
@@ -28,7 +30,7 @@ class Pencil(Stationary):
         super().__init__(title)
 
     def draw(self):
-        return f'Вы взяли {self.title}. Запуск отрисовки карандашом'
+        return f'Запуск отрисовки. {self.title} красиво рисует'
 
 
 class Handle(Stationary):
@@ -36,12 +38,10 @@ class Handle(Stationary):
         super().__init__(title)
 
     def draw(self):
-        return f'Вы взяли {self.title}. Запуск отрисовки маркером'
+        return f'Запуск отрисовки. {self.title} ярко выделяет текст'
 
 
-pen = Pen('Ручка')
-pencil = Pencil('Карандаш')
-handle = Handle('Маркер')
-print(pen.draw())
-print(pencil.draw())
-print(handle.draw())
+SCRIPT_NAME, user_pen, user_pencil, user_hendle = argv
+print(Pen(user_pen).draw())
+print(Pencil(user_pencil).draw())
+print(Handle(user_hendle).draw())
