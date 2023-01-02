d55f36b007b94139c2ed0f880ed1f434f7f1798d
SeregaLi <kaprall13rus@gmail.com>
2023-01-02 19:31:10 +0300

    HW10  исправил задачу с синглтоном

--- HomeWork10/main_3.py
+++ HomeWork10/main_3.py
@@ -1,25 +1,25 @@
 # Домашняя работа по десятому семинару
 # Задание № 3 Создать метакласс для паттерна Синглтон (см. конец вебинара)
 
-class Singleton:
-    _instances = {}
+class Singleton(type):
+    _instance = None
 
-    def __new__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            instance = super().__new__(cls)
-            cls._instances[cls] = instance
-        return cls._instances[cls]
-
-
-class Logger(Singleton):
-    def write_log(self, path):
-        pass
+    def __call__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__call__(*args, **kwargs)
+            return cls._instance
+        return cls._instance
 
 
-if __name__ == "__main__":
-    logger1 = Logger()
-    logger2 = Logger()
-    assert logger1 is logger2
+class Logger(metaclass=Singleton):
+    def __init__(self, text):
+        self.text = text
 
+
+logger1 = Logger('елка')
+logger2 = Logger('Привет')
+print(logger1)
+print(logger2)
+print('Получили ссулку на одну область памяти')
 print(f"logger1 is logger1  {logger1 is logger1}")
 print(f"logger1 is logger1  {logger2 is logger1}")
